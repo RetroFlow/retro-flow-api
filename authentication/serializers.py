e73bacc80ec1d66d1bf87c7481c7b4fc10981ad1
masha <mashakereb@gmail.com>
2018-05-27 12:48:25 +0300

    Fixed user creation method in UserManager

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -6,8 +6,6 @@ from .models import User
 class RegistrationSerializer(serializers.ModelSerializer):
     """Serializers registration requests and creates a new user."""
 
-    # Ensure passwords are at least 8 characters long, no longer than 128
-    # characters, and can not be read by the client.
     password = serializers.CharField(
         max_length=128,
         min_length=8,
@@ -20,10 +18,7 @@ class RegistrationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        # List all of the fields that could possibly be included in a request
-        # or response, including fields specified explicitly above.
         fields = ['email', 'password', 'token', "username"]
 
     def create(self, validated_data):
-        # Use the `create_user` method we wrote earlier to create a new user.
         return User.objects.create_user(**validated_data)
---
+++ board/models/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+
+class ItemStatus(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField(blank=True, null=True)
+    
+
+class BoardSettings(models.Model):
+    sprint_start_date = models.DateField()
+    
+    sprint_duration = models.DurationField()
+    discussion_period = models.DurationField()
+    
+    active_statuses = models.ManyToManyField('ItemStatus')
+    custom_statuses = models.ManyToManyField('ItemStatus')
+    
+    
+class Comment(models.Model):
+    item = models.ForeignKey('Item', related_name='comments', on_delete=models.CASCADE)
+    text = models.TextField(verbose_name='Comment body')
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Item(models.Model):
+    column = models.ForeignKey('Column', related_name='items')
+    heading = models.CharField(max_length=125)
+    
+    description = models.TextField(blank=True, null=True)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    
+class Column(models.Model):
+    name = models.CharField(max_length=40)
+    board = models.ForeignKey('Board', related_name='columns')
+    
+    
+class Board(models.Model):
+    settings = models.OneToOneField('BoardSettings')
+
+    created_at = models.DateTimeField(auto_now_add=True)
