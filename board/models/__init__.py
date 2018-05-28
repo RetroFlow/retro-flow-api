3f5c27259bb19e835a3cf565e9094f6ae4707137
masha <mashakereb@gmail.com>
2018-05-28 11:30:12 +0300

    Models for team-board interactions

--- board/models/__init__.py
+++ board/models/__init__.py
@@ -1,1 +1,6 @@
-from .team import *
+from .team import (
+    Team, Profile, PublicInfo, MembershipInfo, Group, UserRole
+)
+from .board import (
+    BoardSettings, Board, Item, ItemStatus, Assignee, Column, Comment, Vote, Sprint
+)
