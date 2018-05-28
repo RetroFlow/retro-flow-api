5f6f6cf086a41d946138c304cadee1f8ff35e074
masha <mashakereb@gmail.com>
2018-05-28 13:05:30 +0300

    Settings for djsngo deploy

--- retroflow/settings.py
+++ retroflow/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import datetime
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -174,3 +175,4 @@ STATIC_ROOT = '/static/'
 
 
 AUTH_USER_MODEL = 'authentication.User'
+django_heroku.settings(locals())
