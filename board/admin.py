f9bb6fafcd28ff613e1e61c89acc7e5256b28403
masha <mashakereb@gmail.com>
2018-05-28 11:48:28 +0300

    Added new models to the admin site

--- board/admin.py
+++ board/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import(
+    UserRole, ItemStatus, BoardSettings, Board, Profile, Group, MembershipInfo, Item, Assignee, Team, Vote,
+     PublicInfo,  Column, Sprint, Comment
+)
 
-# Register your models here.
+admin.site.register(UserRole)
+admin.site.register(ItemStatus)
+admin.site.register(Board)
+admin.site.register(BoardSettings)
+admin.site.register(Profile)
+admin.site.register(Group)
+admin.site.register(MembershipInfo)
+admin.site.register(Item)
+admin.site.register(Assignee)
+admin.site.register(Team)
+admin.site.register(Vote)
+admin.site.register(Column)
+admin.site.register(Sprint)
+admin.site.register(Comment)
+admin.site.register(PublicInfo)
